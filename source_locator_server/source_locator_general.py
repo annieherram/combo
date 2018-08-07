139859e75cd01b329f3753fdc3d754a3344cbc47
Annie <annieharamatcal@gmail.com>
2018-08-07 14:41:08 +0300

    Added local path import support

--- source_locator_server/source_locator_general.py
+++ source_locator_server/source_locator_general.py
@@ -9,5 +9,7 @@ class ProjectSource:
         if src_type == 'git':
             self.remote_url = kwargs['url']
             self.commit_hash = kwargs['commit_hash']
+        elif src_type == 'local_path':
+            self.local_path = kwargs['path']
         else:
             raise TypeError('Source type {} is not supported yet'.format(src_type))
