41a35f9b19f16b2b698a3872b87c2c69942f5022
Annie Herram <annieharamatcal@gmail.com>
2019-01-20 12:39:33 +0200

    Now contacting server when requested for all sources full json

--- server_communicator.py
+++ server_communicator.py
@@ -1,8 +1,13 @@
 from combo_core.source_locator import *
+from combo_core.compat import connection_error
 import requests
 
 COMBO_SERVER_ADDRESS = ('localhost', 5000)
 MAX_RESPONSE_LENGTH = 4096
+
+
+class ServerConnectionError(ComboException, connection_error):
+    pass
 
 
 class NackFromServer(ComboException):
@@ -12,48 +17,24 @@ class NackFromServer(ComboException):
 class ServerSourceLocator(SourceLocator):
     def __init__(self, address):
         self._addr = address
-
-    def contact_server(self, project_name, version):
-        url = 'http://' + ':'.join(str(x) for x in self._addr)
-        params = {'request_type': 'get_source', 'project_name': project_name, 'project_version': str(version)}
-        response = requests.get(url, params=params)
-
-        try:
-            source = json.loads(response.content.decode())
-        except BaseException as e:
-            raise NackFromServer(e, response)
-
-        return source
+        self._url = 'http://' + ':'.join(str(x) for x in address)
 
     def get_source(self, project_name, version):
+        params = {'project_name': project_name, 'project_version': str(version)}
+
         try:
-            source = self.contact_server(project_name, version)
-        except NackFromServer:
-            raise UndefinedProject('Server could not locate project {} with version {}'.format(project_name, version))
+            response = requests.get('/'.join((self._url, 'get_source')), params=params)
+            source = json.loads(response.content.decode())
+        except BaseException as e:
+            raise UndefinedProject('Server could not locate project "{}" version "{}"'.format(project_name, version), e)
 
         return source
 
     def all_sources(self):
-        def full_json():
-            d = {
-                "(Core Library, v2.1)": {
-                    "hash": 1507179887,
-                    "size": 126
-                },
-                "(Lib A, v1.7)": {
-                    "hash": 501194260,
-                    "size": 229
-                },
-                "(Lib A, v1.6)": {
-                    "hash": 1836199491,
-                    "size": 220
-                },
-                "(Lib B, v1.4)": {
-                    "hash": 1555234999,
-                    "size": 221
-                }
-            }
+        try:
+            response = requests.get('/'.join((self._url, 'get_available_versions')))
+            sources_dict = json.loads(response.content.decode())
+        except BaseException as e:
+            raise ServerConnectionError('Server did not return a list of the available versions', e)
 
-            return json.dumps(d)
-
-        return json.loads(full_json())
+        return sources_dict
